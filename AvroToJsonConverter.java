7988dd7f7c352512e16dab67b15f4e7f2ae21800
kprudhviraj <techprudhvi@gmail.com>
2023-03-10 16:41:20 -0600

    Update AvroToJsonConverter.java

--- AvroToJsonConverter.java
+++ AvroToJsonConverter.java
@@ -1,84 +1,79 @@
-package org.example;
+import org.apache.avro.Schema;
+import org.apache.avro.Schema.Field;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.util.Utf8;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericData;
-import org.apache.avro.generic.GenericDatumWriter;
-import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.io.DatumWriter;
-import org.apache.avro.io.Encoder;
-import org.apache.avro.io.EncoderFactory;
+import java.util.*;
 
 public class AvroToJsonConverter {
+
     public static void main(String[] args) throws IOException {
-
-
-        Schema schema = new Schema.Parser().parse(new File("E:\\HomeLab\\apps\\Microservices\\json2avro\\src\\main\\resources\\data.avsc"));
-
+        String schemaFile = "example.avsc";
+        Schema schema = new Schema.Parser().parse(new File(schemaFile));
         GenericRecord record = new GenericData.Record(schema);
-
-        setDefaultValues(schema, record);
-
-        System.out.println(record);
+        JSONObject json = new JSONObject();
+        convertToJson(record, json);
+        System.out.println(json.toString());
     }
 
-        private static void setDefaultValues(Schema schema, GenericRecord record) {
-            if (schema.getType() == Schema.Type.UNION) {
+    private static void convertToJson(GenericRecord record, JSONObject json) {
+        for (Field field : record.getSchema().getFields()) {
+            String fieldName = field.name();
+            Object fieldValue = record.get(fieldName);
+            Schema fieldSchema = field.schema();
+            if (fieldSchema.getType() == Schema.Type.UNION) {
                 // Handle union types
-                for (Schema unionSchema : schema.getTypes()) {
+                for (Schema unionSchema : fieldSchema.getTypes()) {
                     if (unionSchema.getType() != Schema.Type.NULL) {
-                        setDefaultValues(unionSchema, record);
+                        fieldValue = record.get(fieldName);
                         break;
                     }
                 }
-            } else {
-                for (Schema.Field field : schema.getFields()) {
-                    switch (field.schema().getType()) {
-                        case INT:
-                            record.put(field.name(), 0);
-                            break;
-                        case LONG:
-                            record.put(field.name(), 0L);
-                            break;
-                        case FLOAT:
-                            record.put(field.name(), 0.0f);
-                            break;
-                        case DOUBLE:
-                            record.put(field.name(), 0.0d);
-                            break;
-                        case BOOLEAN:
-                            record.put(field.name(), false);
-                            break;
-                        case STRING:
-                            record.put(field.name(), "");
-                            break;
-                        case RECORD:
-                            GenericRecord childRecord = new GenericData.Record(field.schema());
-                            record.put(field.name(), childRecord);
-                            setDefaultValues(field.schema(), childRecord);
-                            break;
-                        case ARRAY:
-                            GenericData.Array<Object> array = new GenericData.Array<>(0, field.schema());
-                            record.put(field.name(), array);
-                            if (field.schema().getElementType().getType() == Schema.Type.RECORD) {
-                                GenericRecord childArrayRecord = new GenericData.Record(field.schema().getElementType());
-                                array.add(childArrayRecord);
-                                setDefaultValues(field.schema().getElementType(), childArrayRecord);
-                            }
-                            break;
-                        case MAP:
-                            GenericRecord childMapRecord = new GenericData.Record(field.schema().getValueType());
-                            record.put(field.name(), childMapRecord);
-                            setDefaultValues(field.schema().getValueType(), childMapRecord);
-                            break;
-                        default:
-                            record.put(field.name(), null);
+            }
+            if (fieldValue == null) {
+                continue;
+            }
+            switch (fieldSchema.getType()) {
+                case RECORD:
+                    JSONObject subJson = new JSONObject();
+                    convertToJson((GenericRecord) fieldValue, subJson);
+                    json.put(fieldName, subJson);
+                    break;
+                case ARRAY:
+                    List<Object> arrayValues = new ArrayList<>();
+                    for (Object arrayValue : (List<?>) fieldValue) {
+                        if (arrayValue instanceof GenericRecord) {
+                            JSONObject subJsonArrayValue = new JSONObject();
+                            convertToJson((GenericRecord) arrayValue, subJsonArrayValue);
+                            arrayValues.add(subJsonArrayValue);
+                        } else {
+                            arrayValues.add(arrayValue);
+                        }
                     }
-                }
+                    json.put(fieldName, arrayValues);
+                    break;
+                case MAP:
+                    Map<Utf8, Object> mapValues = (Map<Utf8, Object>) fieldValue;
+                    JSONObject subJsonMap = new JSONObject();
+                    for (Map.Entry<Utf8, Object> entry : mapValues.entrySet()) {
+                        if (entry.getValue() instanceof GenericRecord) {
+                            JSONObject subJsonMapValue = new JSONObject();
+                            convertToJson((GenericRecord) entry.getValue(), subJsonMapValue);
+                            subJsonMap.put(entry.getKey().toString(), subJsonMapValue);
+                        } else {
+                            subJsonMap.put(entry.getKey().toString(), entry.getValue());
+                        }
+                    }
+                    json.put(fieldName, subJsonMap);
+                    break;
+                default:
+                    json.put(fieldName, fieldValue);
+                    break;
             }
         }
     }
-
-
+}
