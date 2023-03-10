3d3db6f0326212901ecd323f4e53fecbf801db10
kprudhviraj <techprudhvi@gmail.com>
2023-03-10 17:18:29 -0600

    Update AvroToJsonConverter.java

--- AvroToJsonConverter.java
+++ AvroToJsonConverter.java
@@ -1,107 +1,76 @@
-import org.apache.avro.Schema;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.ByteBuffer;
+import org.apache.avro.Schema;
+import org.apache.avro.Schema.Type;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.Encoder;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.specific.SpecificDatumWriter;
 
-public class AvroToJsonConverter {
+public class AvroGenerator {
+
     public static void main(String[] args) throws IOException {
-        // Read the Avro schema from a file
-        Schema schema = new Schema.Parser().parse(new File("example.avsc"));
+        File schemaFile = new File("path/to/schema.avsc");
+        Schema schema = new Schema.Parser().parse(schemaFile);
 
-        // Convert the schema to a JSON object
-        JSONObject schemaJson = toJson(schema);
-
-        System.out.println(schemaJson.toString(4));
-    }
-
-    private static JSONObject toJson(Schema schema) {
-        JSONObject json = new JSONObject();
-
-        if (schema.getFields() != null && !schema.getFields().isEmpty()) {
-            List<JSONObject> fieldsJson = new ArrayList<>();
-
-            for (Schema.Field field : schema.getFields()) {
-                JSONObject fieldJson = getFieldJson(field);
-
-                if (fieldJson != null) {
-                    fieldsJson.add(fieldJson);
-                }
-            }
-
-            if (!fieldsJson.isEmpty()) {
-                json.put("fields", new JSONArray(fieldsJson));
-            }
+        GenericRecord record = new GenericData.Record(schema);
+        for (Schema.Field field : schema.getFields()) {
+            Object defaultValue = getDefaultValue(field.schema());
+            record.put(field.name(), defaultValue);
         }
 
-        return json;
+        DatumWriter<GenericRecord> datumWriter = new SpecificDatumWriter<>(schema);
+        Encoder encoder = EncoderFactory.get().binaryEncoder(System.out, null);
+        datumWriter.write(record, encoder);
+        encoder.flush();
     }
 
-    private static JSONObject getFieldJson(Schema.Field field) {
-        JSONObject json = new JSONObject();
-
-        json.put("name", field.name());
-
-        Schema fieldSchema = field.schema();
-
-        if (fieldSchema.isUnion()) {
-            fieldSchema = getDefaultUnionType(fieldSchema);
-        }
-
-        Schema.Type fieldType = fieldSchema.getType();
-
-        switch (fieldType) {
-            case INT:
-            case LONG:
-            case FLOAT:
-            case DOUBLE:
-                json.put("default", 0);
-                break;
-
-            case BOOLEAN:
-                json.put("default", false);
-                break;
-
-            case STRING:
-            case BYTES:
-                json.put("default", "");
-                break;
-
-            case ENUM:
-                List<String> symbols = fieldSchema.getEnumSymbols();
-                json.put("default", symbols.get(0));
-                break;
-
-            case RECORD:
+    private static Object getDefaultValue(Schema schema) {
+        Type type = schema.getType();
+        switch (type) {
             case ARRAY:
+                return new GenericData.Array<>(0, schema);
+            case BOOLEAN:
+                return false;
+            case BYTES:
+                return ByteBuffer.wrap(new byte[0]);
+            case DOUBLE:
+                return 0.0;
+            case ENUM:
+                return schema.getEnumSymbols().get(0);
+            case FIXED:
+                return new GenericData.Fixed(schema);
+            case FLOAT:
+                return 0.0f;
+            case INT:
+                return 0;
+            case LONG:
+                return 0L;
             case MAP:
-                json = null;
-                break;
-
+                return new GenericData.StringMap<>(0);
+            case NULL:
+                return null;
+            case RECORD:
+                GenericRecord record = new GenericData.Record(schema);
+                for (Schema.Field field : schema.getFields()) {
+                    Object defaultValue = getDefaultValue(field.schema());
+                    record.put(field.name(), defaultValue);
+                }
+                return record;
+            case STRING:
+                return "";
+            case UNION:
+                for (Schema unionSchema : schema.getTypes()) {
+                    if (!unionSchema.getType().equals(Type.NULL)) {
+                        return getDefaultValue(unionSchema);
+                    }
+                }
+                return null;
             default:
-                break;
+                throw new IllegalArgumentException("Unknown schema type: " + type);
         }
-
-        return json;
-    }
-
-    private static Schema getDefaultUnionType(Schema schema) {
-        Schema defaultType = null;
-
-        for (Schema type : schema.getTypes()) {
-            if (type.getType() != Schema.Type.NULL) {
-                defaultType = type;
-                break;
-            }
-        }
-
-        if (defaultType == null) {
-            throw new RuntimeException("Unable to determine default union type for schema: " + schema);
-        }
-
-        return defaultType;
     }
 }
