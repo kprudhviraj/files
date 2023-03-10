ba7c6208258913515b57cb30c5241d2cdb6fed04
kprudhviraj <techprudhvi@gmail.com>
2023-03-10 17:01:21 -0600

    Update AvroToJsonConverter.java

--- AvroToJsonConverter.java
+++ AvroToJsonConverter.java
@@ -1,79 +1,114 @@
 import org.apache.avro.Schema;
-import org.apache.avro.Schema.Field;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.util.Utf8;
-import org.json.JSONObject;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.node.JsonNodeFactory;
+import org.codehaus.jackson.node.ObjectNode;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AvroToJsonConverter {
+    private static final ObjectMapper mapper = new ObjectMapper();
+    private static final JsonNodeFactory nodeFactory = JsonNodeFactory.instance;
 
     public static void main(String[] args) throws IOException {
-        String schemaFile = "example.avsc";
-        Schema schema = new Schema.Parser().parse(new File(schemaFile));
-        GenericRecord record = new GenericData.Record(schema);
-        JSONObject json = new JSONObject();
-        convertToJson(record, json);
-        System.out.println(json.toString());
+        // Read the Avro schema from a file
+        Schema schema = new Schema.Parser().parse(new File("example.avsc"));
+
+        // Convert the schema to a JSON object
+        ObjectNode schemaJson = toJson(schema);
+
+        System.out.println(schemaJson);
     }
 
-    private static void convertToJson(GenericRecord record, JSONObject json) {
-        for (Field field : record.getSchema().getFields()) {
-            String fieldName = field.name();
-            Object fieldValue = record.get(fieldName);
-            Schema fieldSchema = field.schema();
-            if (fieldSchema.getType() == Schema.Type.UNION) {
-                // Handle union types
-                for (Schema unionSchema : fieldSchema.getTypes()) {
-                    if (unionSchema.getType() != Schema.Type.NULL) {
-                        fieldValue = record.get(fieldName);
-                        break;
-                    }
+    private static ObjectNode toJson(Schema schema) {
+        ObjectNode json = nodeFactory.objectNode();
+
+        if (schema.getFields() != null && !schema.getFields().isEmpty()) {
+            List<ObjectNode> fieldsJson = new ArrayList<>();
+
+            for (Schema.Field field : schema.getFields()) {
+                ObjectNode fieldJson = getFieldJson(field);
+
+                if (fieldJson != null) {
+                    fieldsJson.add(fieldJson);
                 }
             }
-            if (fieldValue == null) {
-                continue;
-            }
-            switch (fieldSchema.getType()) {
-                case RECORD:
-                    JSONObject subJson = new JSONObject();
-                    convertToJson((GenericRecord) fieldValue, subJson);
-                    json.put(fieldName, subJson);
-                    break;
-                case ARRAY:
-                    List<Object> arrayValues = new ArrayList<>();
-                    for (Object arrayValue : (List<?>) fieldValue) {
-                        if (arrayValue instanceof GenericRecord) {
-                            JSONObject subJsonArrayValue = new JSONObject();
-                            convertToJson((GenericRecord) arrayValue, subJsonArrayValue);
-                            arrayValues.add(subJsonArrayValue);
-                        } else {
-                            arrayValues.add(arrayValue);
-                        }
-                    }
-                    json.put(fieldName, arrayValues);
-                    break;
-                case MAP:
-                    Map<Utf8, Object> mapValues = (Map<Utf8, Object>) fieldValue;
-                    JSONObject subJsonMap = new JSONObject();
-                    for (Map.Entry<Utf8, Object> entry : mapValues.entrySet()) {
-                        if (entry.getValue() instanceof GenericRecord) {
-                            JSONObject subJsonMapValue = new JSONObject();
-                            convertToJson((GenericRecord) entry.getValue(), subJsonMapValue);
-                            subJsonMap.put(entry.getKey().toString(), subJsonMapValue);
-                        } else {
-                            subJsonMap.put(entry.getKey().toString(), entry.getValue());
-                        }
-                    }
-                    json.put(fieldName, subJsonMap);
-                    break;
-                default:
-                    json.put(fieldName, fieldValue);
-                    break;
+
+            if (!fieldsJson.isEmpty()) {
+                json.put("fields", mapper.valueToTree(fieldsJson));
             }
         }
+
+        return json;
+    }
+
+    private static ObjectNode getFieldJson(Schema.Field field) {
+        ObjectNode json = nodeFactory.objectNode();
+
+        json.put("name", field.name());
+
+        Schema fieldSchema = field.schema();
+
+        if (fieldSchema.isUnion()) {
+            fieldSchema = getDefaultUnionType(fieldSchema);
+        }
+
+        Schema.Type fieldType = fieldSchema.getType();
+
+        switch (fieldType) {
+            case INT:
+            case LONG:
+            case FLOAT:
+            case DOUBLE:
+                json.put("default", 0);
+                break;
+
+            case BOOLEAN:
+                json.put("default", false);
+                break;
+
+            case STRING:
+            case BYTES:
+                json.put("default", "");
+                break;
+
+            case ENUM:
+                List<String> symbols = fieldSchema.getEnumSymbols();
+                json.put("default", symbols.get(0));
+                break;
+
+            case RECORD:
+            case ARRAY:
+            case MAP:
+                json = null;
+                break;
+
+            default:
+                break;
+        }
+
+        return json;
+    }
+
+    private static Schema getDefaultUnionType(Schema schema) {
+        Schema defaultType = null;
+
+        for (Schema type : schema.getTypes()) {
+            if (type.getType() != Schema.Type.NULL) {
+                defaultType = type;
+                break;
+            }
+        }
+
+        if (defaultType == null) {
+            throw new RuntimeException("Unable to determine default union type for schema: " + schema);
+        }
+
+        return defaultType;
     }
 }
